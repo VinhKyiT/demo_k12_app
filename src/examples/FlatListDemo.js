c6f37016dc4bde20ebd044424b20e4fc5a877adb
VinhKyIT <vinhkyit2905@gmail.com>
2023-08-21 20:57:33 +0700

    Buoi 22_2

--- src/examples/FlatListDemo.js
+++ src/examples/FlatListDemo.js
@@ -1,5 +1,5 @@
-import { View, Text, FlatList } from 'react-native';
-import React, { useEffect, useRef } from 'react';
+import { View, Text, FlatList, ActivityIndicator, RefreshControl } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
 
 const DS_LOP = [
   { id: '1', name: 'Vĩnh Kỳ', age: 20 },
@@ -16,30 +16,75 @@ const DS_LOP = [
   // ...
 ];
 
+const DS_LOP2 = [
+  { id: '1', name: 'Bùi Phạm Vĩnh Kỳ', age: 20 },
+  { id: '2', name: 'La Quốc Lương', age: 21 },
+  { id: '3', name: 'Lê Ngọc Dũng', age: 22 },
+  { id: '4', name: 'Thanh Tùng', age: 23 },
+  { id: '5', name: 'Quốc Thanh', age: 24 },
+  { id: '6', name: 'Đình Thọ', age: 25 },
+  { id: '7', name: 'Lâm Phương', age: 26 },
+  { id: '8', name: 'Hanh', age: 27 },
+  { id: '9', name: 'Trung Phát', age: 28 },
+  { id: '10', name: 'Minh Thuận', age: 29 },
+  { id: '11', name: 'Quang Thực', age: 30 },
+  // ...
+];
+
 const FlatListDemo = () => {
-  const flatListRef = useRef(null);
+  const [data, setData] = useState([]);
+  const [isFetching, setIsFetching] = useState(true);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const fetchUser = async () => {
+    try {
+      const result = await fetch('https://api.escuelajs.co/api/v1/users');
+      const resultJson = await result.json();
+      return resultJson;
+    } catch (error) {
+      console.log({ error });
+    }
+  };
 
   useEffect(() => {
-    setTimeout(() => {
-      flatListRef?.current?.scrollToIndex({
-        index: 3,
-        animated: true,
-      });
-    }, 1000);
+    setIsFetching(true);
+    fetchUser()
+      .then(res => setData(res))
+      .catch(err => console.log({ err }))
+      .finally(() => setIsFetching(false));
   }, []);
+
+  const handleRefresh = () => {
+    if (isRefreshing) {
+      return;
+    }
+    console.log('handleRefresh');
+    setIsRefreshing(true);
+    fetchUser()
+      .then(res => setData(res))
+      .catch(err => console.log({ err }))
+      .finally(() => setIsRefreshing(false));
+  };
+
+  console.log('isFetching', isFetching);
 
   return (
     <View style={{ flex: 1 }}>
       <FlatList
-        ref={flatListRef}
-        data={DS_LOP}
+        data={data}
         keyExtractor={item => item.id}
+        refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />}
         ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
-        ListEmptyComponent={() => (
-          <View style={{ alignItems: 'center' }}>
-            <Text>Danh sách rỗng</Text>
-          </View>
-        )}
+        ListEmptyComponent={() => {
+          if (isFetching) {
+            return <ActivityIndicator size={'large'} />;
+          }
+          return (
+            <View style={{ alignItems: 'center' }}>
+              <Text>Danh sách rỗng</Text>
+            </View>
+          );
+        }}
         ListHeaderComponent={() => (
           <View style={{ alignItems: 'center', marginVertical: 16 }}>
             <Text style={{ color: 'black', fontSize: 20, fontWeight: '500' }}>
@@ -47,12 +92,17 @@ const FlatListDemo = () => {
             </Text>
           </View>
         )}
-        ListFooterComponent={() => (
-          <View style={{ alignItems: 'center', marginVertical: 16 }}>
-            <Text>Đã hết</Text>
-          </View>
-        )}
-        contentContainerStyle={{}}
+        ListFooterComponent={() => {
+          if (isFetching) {
+            return null;
+          }
+          return (
+            <View style={{ alignItems: 'center', marginVertical: 16 }}>
+              <Text>Đã hết</Text>
+            </View>
+          );
+        }}
+        contentContainerStyle={{ paddingHorizontal: 16 }}
         renderItem={({ item, index }) => (
           <View
             style={{
@@ -66,8 +116,8 @@ const FlatListDemo = () => {
               <Text>{item.name}</Text>
             </View>
             <View style={{ flexDirection: 'row', marginTop: 4 }}>
-              <Text>Tuổi: </Text>
-              <Text>{item.age}</Text>
+              <Text>Email: </Text>
+              <Text>{item.email}</Text>
             </View>
           </View>
         )}
--- src/routes/MainNavigator.js
+++ src/routes/MainNavigator.js
@@ -8,6 +8,7 @@ import TodoScreen from '../screens/Todo';
 import TodoListScreen from '../screens/TodoList';
 import TaskDetailScreen from '../screens/TaskDetail';
 import FlatListDemo from '../examples/FlatListDemo';
+import SectionListDemo from '../examples/SectionListDemo';
 
 const Stack = createStackNavigator();
 
@@ -20,6 +21,7 @@ function MainNavigator() {
         <Stack.Screen name="TodoList" component={TodoListScreen} />
         <Stack.Screen name="TaskDetail" component={TaskDetailScreen} />
         <Stack.Screen name="FlatListDemo" component={FlatListDemo} />
+        <Stack.Screen name="SectionListDemo" component={SectionListDemo} />
         <Stack.Screen name="Detail" component={DetailScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
