8362845d33d674765d2b34b0264a053b4c926a56
VinhKyIT <vinhkyit2905@gmail.com>
2023-09-13 20:59:59 +0700

    Buoi 26

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MainNavigator from './src/routes/MainNavigator';
+import AuthContextProvider from '~contexts/AuthContext/AuthContextProvider';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <GestureHandlerRootView style={{ flex: 1 }}>
         <View style={{ flex: 1 }}>
-          <MainNavigator />
+          <AuthContextProvider>
+            <MainNavigator />
+          </AuthContextProvider>
         </View>
       </GestureHandlerRootView>
     </View>
--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Image, Text, TouchableOpacity } from 'react-native';
-
-const TaskItem = ({ item, index, onItemClick, onItemTickBoxClick }) => {
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+const TaskItem = ({ item, index, onItemClick, onItemTickBoxClick, onDeleteButtonClick }) => {
   return (
     <TouchableOpacity
       onPress={() => onItemClick(item)}
@@ -12,21 +12,29 @@ const TaskItem = ({ item, index, onItemClick, onItemTickBoxClick }) => {
         elevation: 5,
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between',
         marginBottom: 16,
       }}>
-      <TouchableOpacity
-        onPress={() => onItemTickBoxClick(index)}
-        style={{
-          width: 24,
-          height: 24,
-          backgroundColor: 'rgba(85, 188, 246, 0.4)',
-          borderRadius: 5,
-        }}>
-        {item.status === 'done' ? (
-          <Image source={require('../assets/images/icon_check.png')} />
-        ) : null}
-      </TouchableOpacity>
-      <Text style={{ marginLeft: 8, color: '#1A1A1A' }}>{item?.title}</Text>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <TouchableOpacity
+          onPress={() => onItemTickBoxClick(item?.id)}
+          style={{
+            width: 24,
+            height: 24,
+            backgroundColor: 'rgba(85, 188, 246, 0.4)',
+            borderRadius: 5,
+          }}>
+          {item.status === 'done' ? (
+            <Image source={require('../assets/images/icon_check.png')} />
+          ) : null}
+        </TouchableOpacity>
+        <Text style={{ marginLeft: 8, color: '#1A1A1A' }}>{item?.title}</Text>
+      </View>
+      {item.status === 'done' ? (
+        <TouchableOpacity onPress={() => onDeleteButtonClick?.(item?.id)}>
+          <Feather name="trash-2" size={24} color={'#ff0000'} />
+        </TouchableOpacity>
+      ) : null}
     </TouchableOpacity>
   );
 };
