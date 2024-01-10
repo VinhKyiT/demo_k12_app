c0bd2f4ae3bfa9f0d8dc08eab20a4823e4eb5e3a
VinhKyIT <vinhkyit2905@gmail.com>
2024-01-10 20:43:51 +0700

    buoi_52

---
+++ android/app/src/main/java/com/nestechk12hcm/CallingModule/CallingModule.java
@@ -0,0 +1,49 @@
+package com.nestechk12hcm.CallingModule;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.util.Map;
+import java.util.HashMap;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+
+public class CallingModule extends ReactContextBaseJavaModule {
+  CallingModule(ReactApplicationContext context) {
+    super(context);
+  }
+
+  @Override
+  public String getName() {
+    return "CallingModule";
+  }
+
+  @ReactMethod
+  public void makeCall(String phoneNumber) {
+    Log.d("CallingModule", "Calling to number: " + phoneNumber);
+    ReactApplicationContext ctx = getReactApplicationContext();
+    TelecomManager tm = (TelecomManager) ctx.getSystemService(Context.TELECOM_SERVICE);
+
+    Uri uri = Uri.fromParts("tel", phoneNumber, null);
+    Bundle extras = new Bundle();
+    extras.putBoolean(TelecomManager.EXTRA_START_CALL_WITH_SPEAKERPHONE, true);
+    if (ActivityCompat.checkSelfPermission(ctx, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+      return;
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+      tm.placeCall(uri, extras);
+    }
+  }
+}
